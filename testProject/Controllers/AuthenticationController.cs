c5f2ed4eb4b62261bed8b06564f59cb8cc9014b3
Yaroslav Postelnykov <116905181+bl1ndHarper@users.noreply.github.com>
2023-12-06 19:43:35 +0200

    Users authentication: Registration and Sign In pages UI (#2)
    * Sign Up and Log In Pages front added
    
    * Shared Layout fixed - Top Bar items float is now right

---
+++ testProject/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testProject.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult SignUpPage()
+        {
+            return View();
+        }
+        public IActionResult LogInPage()
+        {
+            return View();
+        }
+    }
+}
