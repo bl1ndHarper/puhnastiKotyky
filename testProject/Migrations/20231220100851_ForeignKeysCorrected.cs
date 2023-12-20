2ed6da54365433cd616722efcc88dea384e88c5f
AnnMyroshnichenko <123562652+AnnMyroshnichenko@users.noreply.github.com>
2023-12-20 19:58:17 +0200

    Migration to correct foreign keys (#11)

---
+++ testProject/Migrations/20231220100851_ForeignKeysCorrected.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace testProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class ForeignKeysCorrected : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "projects_users_projects_users_id_foreign",
+                table: "projects_users");
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "projects_users_id",
+                table: "projects_users",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "users_technologies_id",
+                table: "users_technologies",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_projects_users_users_id",
+                table: "projects_users",
+                column: "users_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "projects_users_users_id_foreign",
+                table: "projects_users",
+                column: "users_id",
+                principalTable: "users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "projects_users_users_id_foreign",
+                table: "projects_users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_projects_users_users_id",
+                table: "projects_users");
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "projects_users_id",
+                table: "projects_users",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddForeignKey(
+                name: "projects_users_projects_users_id_foreign",
+                table: "projects_users",
+                column: "projects_users_id",
+                principalTable: "users",
+                principalColumn: "Id");
+        }
+    }
+}
