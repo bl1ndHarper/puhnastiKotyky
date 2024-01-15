b3e76a38c4aadc23d1e82ca74cee503174495bc6
bl1ndHarper <yarikpost@gmail.com>
2024-01-15 19:18:56 +0200

    Public project page controller: create request method

--- testProject/Areas/Projects/Controllers/PublicController.cs
+++ testProject/Areas/Projects/Controllers/PublicController.cs
@@ -1,31 +1,87 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Evaluation;
 using Microsoft.CodeAnalysis;
+using System.Security.Claims;
+using testProject.Data;
+using testProject.Models;
 
 namespace testProject.Areas.Projects.Controllers
 {
     [Area("Projects")]
     [Route("Projects/Public/[action]")]
+    [Authorize]
     public class PublicController : Controller
     {
+        private AppDbContext _db;
+
+        public PublicController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult CreateNewRequest()
+        public ActionResult CreateNewRequest(string projectsId)
         {
             try
             {
-                // ...
-                int[] array = { 1 };
-                Console.WriteLine(array[1]);
-                return Json(new { success = true, message = "Successfully requested to be a part of the team!" });
+                _db = new AppDbContext();
+                var userId = Convert.ToUInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+                // searching for existing request with given parameters and status != "deleted"
+                var checkRequest = _db.Requests
+                            .Where(r => r.UsersId == userId
+                            && r.ProjectsId == Convert.ToUInt32(projectsId)
+                            && r.Status != "deleted")
+                            .FirstOrDefault();
+
+                if (checkRequest != null && checkRequest.Status == "considering")       // if it exists user cannot create its copy
+                {
+                    return Json(new { success = false, message = "You've already submitted a request for this project. Delete it to resubmit or contact the project owner." });
+                }
+                else if (checkRequest != null && checkRequest.Status == "accepted")     // if user is in the team they also can't submit a request
+                {
+                    return Json(new { success = false, message = "You're already a part of this project's team. " });
+                }
+                else if (checkRequest == null)  // if it doesn't exist create one
+                {
+                    Models.Request request = new Models.Request
+                    { 
+                        UsersId = userId,
+                        ProjectsId = Convert.ToUInt32(projectsId),
+                        Date = DateTime.Now,
+                        Status = "considering",
+                        IsHidden = false
+                    };
+                    _db.Requests.Add(request);
+                    _db.SaveChanges();
+                    // check if the creation succeeded
+                    var requestId = request.RequestsId;
+                    checkRequest = _db.Requests
+                                .Where(r => r.RequestsId == Convert.ToUInt32(requestId))
+                                .FirstOrDefault();
+                    if (checkRequest != null)
+                    {
+                        return Json(new { success = true, message = "Successfully requested to be a part of the team!" });
+                    } else
+                    {
+                        return Json(new { success = false, message = "An error occurred while sending a request. Try again later." });
+                    }
+                }
+                else    // I dunno what else can go wrong
+                {
+                    return Json(new { success = false, message = "An error occurred while sending a request. Try again later." });
+                }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                // ...
-                return Json(new { success = false, message = "An error occurred while sending a request. Try again later."});
+                return Json(new { success = false, message = "An error occurred while sending a request: " + ex.Message });
             }
         }
     }
