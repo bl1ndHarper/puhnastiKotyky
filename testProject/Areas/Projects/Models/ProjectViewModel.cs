1516102060398db5d396b2e769dc576813171ced
AnnMyroshnichenko <123562652+AnnMyroshnichenko@users.noreply.github.com>
2024-01-15 19:17:22 +0200

    ViewModel for the project page (#36)
    * ViewModel for the project page
    
    * Removed user id parameter from the Index method

---
+++ testProject/Areas/Projects/Models/ProjectViewModel.cs
@@ -0,0 +1,12 @@
+﻿using testProject.Models;
+
+namespace testProject.Areas.Projects.Models
+{
+    public class ProjectViewModel
+    {
+        public Project Project { get; set; }
+        public IEnumerable<Technology> Technologies { get; set; }
+        public IEnumerable<User> Team { get; set; }
+
+    }
+}
