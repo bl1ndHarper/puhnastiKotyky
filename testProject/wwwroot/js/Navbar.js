da3eb99213d20d58e585a958fbdd4dbca81035e9
bl1ndHarper <yarikpost@gmail.com>
2024-01-19 20:15:09 +0200

    Main navigation bar

---
+++ testProject/wwwroot/js/Navbar.js
@@ -0,0 +1,9 @@
+﻿const tabs = document.querySelectorAll(".navbar__tab-item");
+
+var currentPage = window.location.pathname;
+if (currentPage.search("Account/Index") != -1) {
+    Array.from(tabs).at(1).style.background = "#8EC3B0";
+}
+else if (currentPage == "/") {
+    Array.from(tabs).at(0).style.background = "#8EC3B0";
+}
