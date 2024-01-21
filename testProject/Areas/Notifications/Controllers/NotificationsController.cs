ce743cb130d9e33e888a73f2679ffb06083257ae
Yaroslav Postelnykov <116905181+bl1ndHarper@users.noreply.github.com>
2024-01-21 13:44:12 +0200

    Notifications page (#41)
    Notifications area, page and controller created. Javascript functions for creating and deleting notification items too.

---
+++ testProject/Areas/Notifications/Controllers/NotificationsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testProject.Areas.Notifications.Controllers
+{
+    [Area("Notifications")]
+    public class NotificationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
