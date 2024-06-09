c8d39df5b3dfaeecd36a618bd832cce75005734f
Yaroslav Postelnykov <yarikpost@gmail.com>
2024-06-09 13:22:04 +0300

    Public project and Notifications pages adapted for mobile + home page carousel adjusted (#71)

--- testProject/wwwroot/js/ProjectPage.js
+++ testProject/wwwroot/js/ProjectPage.js
@@ -1,20 +1,60 @@
-﻿window.onload = function () {
+﻿const repoLink = document.querySelector(".project__repository-link:not(.mobile)");
+const repoLinkMobile = document.querySelector(".mobile");
+
+window.onload = function () {
     const body = document.querySelector(".project__body");
     const header = document.querySelector(".project__header");
-    var headerHeight = header.clientHeight;
     const title = header.querySelector("h1");
-    const projectInfo = header.querySelector("div");
+    const projectInfo = header.querySelector("div:not(.project__repository-link)");
+
+    setTimeout(() => {
+        if (repoLinkMobile != null) repoLinkMobile.querySelector("a").style.display = "none";
+    }, 3000);
+
+    handleMediaResult();
     
     body.onscroll = function () {
         if (body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            //projectInfo.style.opacity = 0;
             projectInfo.classList.add("hidden");
-            title.style.marginTop = "50px";
+            title.style.marginTop = "60px";
+            repoLinkMobile.classList.add("fixed-bottom");
         } else {
-            //projectInfo.style.opacity = 1;
             projectInfo.classList.remove("hidden");
             title.style.marginTop = "0";
+            repoLinkMobile.classList.remove("fixed-bottom");
         }
+    }
+
+    let resizeObserver = new ResizeObserver(() => {
+        handleMediaResult();
+    });
+
+    resizeObserver.observe(body);
+
+    function handleMediaResult() {
+        if (repoLink != null) {
+            if (window.innerWidth <= 600) {
+                if (!repoLink.classList.contains("hidden")) {
+                    repoLink.classList.add("hidden");
+                    repoLinkMobile.classList.remove("hidden");
+                }
+            } else {
+                if (repoLink.classList.contains("hidden")) {
+                    repoLink.classList.remove("hidden");
+                    repoLinkMobile.classList.add("hidden");
+                }
+            }
+        }
+    }
+
+    if (repoLinkMobile != null) {
+        repoLinkMobile.addEventListener("click", () => {
+            const a = repoLinkMobile.querySelector("a");
+            if (a.style.display == "none") {
+                a.style.display = "flex";
+                setTimeout(() => { repoLinkMobile.querySelector("a").style.display = "none"; }, 3000);
+            }
+        });
     }
 }
 
